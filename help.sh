cafff9248adfa2cf705fe2c192f638a54110f882
Piotr Sobociński <C:\Users\piotr\AppData\Roaming\The Bat!>
2023-06-28 11:39:47 +0200

    --help

---
+++ help.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$#" -eq 1 ] && [ "$1" == "--help" ]; then
+  echo "Dostepne opcje:"
+  echo "--logs, -l <liczba> : utworz automatucznie okreslona liczbe plikow"
+  echo "--date, -d : wyswietla date."
+  echo "--help, -h : wyswietla dostepne opcje"
+  exit 0
+fi
+
+echo "Użyj --help"
+exit 1
